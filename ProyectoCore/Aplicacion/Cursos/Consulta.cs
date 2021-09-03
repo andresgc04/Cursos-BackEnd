60f18ea75f378646f40fe132afea09a85b51f6ff
andresgc04 <andresgc1997@outlook.com>
2021-09-03 00:18:43 -0400

    Configurando el MediatR en nuestra Solucion

---
+++ ProyectoCore/Aplicacion/Cursos/Consulta.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using System.Collections.Generic;
+using Dominio;
+using Persistencia;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplicacion.Cursos
+{
+    public class Consulta
+    {
+        public class ListaCursos : IRequest<List<Curso>> {}
+
+        public class Manejador : IRequestHandler<ListaCursos, List<Curso>>
+        {
+            //Inyecion de dependencia:
+            private readonly CursosOnlineContext _context;
+
+            public Manejador(CursosOnlineContext context){
+                _context = context;
+            }
+
+            public async Task<List<Curso>> Handle(ListaCursos request, CancellationToken cancellationToken)
+            {
+                var cursos = await _context.Curso.ToListAsync();
+                return cursos;
+            }
+        }
+    }
+}
